a952189d816e53a9924cb49b2cbdab5463265742
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 17:19:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,9 +8,9 @@ INSERT INTO books (title, year, series_id) VALUES ("Surgery", 2001, 2);
 INSERT INTO books (title, year, series_id) VALUES ("Hospital Romance", 1998, 2);
 INSERT INTO books (title, year, series_id) VALUES ("Oh NO", 2014, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Harry Potter", "The Boy Who Lived", "Wizard", 1, 1);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "The Gentle Giant", "Giant", 1, 1);
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Fluffy", "A bit of music puts em right to sleep", "Three-Headed Dog", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Ice-T", "Arnold Palmer", "Cop", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Hagrid", "The Gentle Giant", "Cop", 1, 1);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Fluffy", "A bit of music puts em right to sleep", "Cop", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Gilderoy Lockhart", "Me!", "Wizard", 1, 1);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Jackson Avery", "Attractive Doc", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Medideth Grey", "The Woke Doc", "Human", 2, 2);
