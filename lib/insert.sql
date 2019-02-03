6da562cfba38598553a453221cc3228a22fe3243
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 17:12:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Law and Order: SVU", 1, 1);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Grey's Anatomy", 2, 2);
 
-INSERT INTO books (title, year, series_id) VALUES ("The Fellowship of the Ring", 1974, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Two Towers", 1984, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Return of the King", 1999, 1);
-INSERT INTO books (title, year, series_id) VALUES ("The Sorcerer's Stone", 1997, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Disappearance", 1974, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Who did it?", 1984, 1);
+INSERT INTO books (title, year, series_id) VALUES ("I-5 Killer", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("", 2001, 2);
 INSERT INTO books (title, year, series_id) VALUES ("The Chamber of Secrets", 1998, 2);
-INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2000, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Goblet of Fire", 2014, 2);
