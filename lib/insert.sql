1c2b6d500f4332001cf6ef77d63ae06c2ea4a9c7
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-03 17:09:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Trevor Noah Autobiography", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Halsey Street", 2, 2);
